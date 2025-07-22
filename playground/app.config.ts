a04d7dd18ebd635f668b37b4adc9e41d2c1aee57
manchenkoff <artem@manchenkoff.me>
2025-07-22 22:18:12 +0100

    fix: bump packages and fixed vulnerability

--- playground/app.config.ts
+++ playground/app.config.ts
@@ -1,7 +1,15 @@
+import type { NuxtApp } from '#app'
+import type { FetchContext } from 'ofetch'
+import type { ConsolaInstance } from 'consola'
+
 export default defineAppConfig({
   echo: {
     interceptors: {
-      async onRequest(_app, ctx, logger) {
+      async onRequest(
+        _app: NuxtApp,
+        ctx: FetchContext,
+        logger: ConsolaInstance
+      ): Promise<void> {
         const tenant = 'random-string'
 
         ctx.options.headers.set('X-Echo-Tenant', tenant)
