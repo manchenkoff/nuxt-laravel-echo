f312eea93a3a236c47e5c35f271d42f12c33957d
manchenkoff <artem@manchenkoff.me>
2025-10-07 22:08:58 +0100

    chore(deps): bump packages

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,6 +14,6 @@ export default createConfigForNuxt({
     '@stylistic/comma-dangle': 'off',
     '@stylistic/indent': 'off',
     'vue/no-multiple-template-root': 'off',
-    'eslintvue/multi-word-component-names': 'off',
+    'vue/multi-word-component-names': 'off',
   },
 })
